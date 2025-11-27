740854d032713b97ef654079343c79360ead2325
lushunming <1357197829@qq.com>
2025-11-27 10:30:07 +0800

    uc修改

--- src/main/java/com/github/catvod/api/UCApi.java
+++ src/main/java/com/github/catvod/api/UCApi.java
@@ -226,7 +226,7 @@ public class UCApi {
 
             return Result.get().url(playUrl).string();
         }*/
-        return Result.get().url(ProxyServer.INSTANCE.buildProxyUrl(playUrl, new HashMap<>())).octet().header(header).string();
+        return Result.get().url(ProxyServer.INSTANCE.buildProxyUrl(playUrl, new HashMap<>())).string();
     }
 
     private boolean testVideo(String url) {
