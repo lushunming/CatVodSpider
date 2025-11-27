2a4f576a6095ff2b313618949ec732880d9a8d71
lushunming <1357197829@qq.com>
2025-11-27 09:43:04 +0800

    uc修改

--- src/main/java/com/github/catvod/spider/SeedHub.kt
+++ src/main/java/com/github/catvod/spider/SeedHub.kt
@@ -166,7 +166,7 @@ class SeedHub : Cloud() {
         runBlocking {
             doc.select("ul.pan-links > li > a")
                 .filter {a-> a.attr("data-link").contains("quark") || a.attr("data-link").contains("baidu")|| a.attr("data-link").contains("uc") }
-                .slice(IntRange(0, 10)).forEach { element ->
+                .slice(IntRange(0, 5)).forEach { element ->
 
                 jobs += CoroutineScope(Dispatchers.IO).launch {
                     var link = siteUrl + element.attr("href")
