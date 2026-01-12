36863113a322808b3f2536f5fcb01d676ec27344
lushunming <1357197829@qq.com>
2026-01-12 10:39:24 +0800

    夸克代理优化

--- src/main/java/com/github/catvod/api/QuarkApi.java
+++ src/main/java/com/github/catvod/api/QuarkApi.java
@@ -204,10 +204,7 @@ public class QuarkApi {
             return Result.get().url(ProxyServer.INSTANCE.buildProxyUrl(playUrl, header)).octet().header(header).string();
         } else {
             playUrl = this.getLiveTranscoding(shareId, stoken, fileId, fileToken, flag);
-            if (playUrl != null && Util.getExt(playUrl).contains("m3u8")) {
-                return Result.get().url(Proxy.buildProxyUrl("quark", playUrl, header, "video")).octet().header(header).string();
-            }
-            return Result.get().url(ProxyServer.INSTANCE.buildProxyUrl(playUrl, header)).octet().header(header).string();
+            return Result.get().url(Proxy.buildProxyUrl("quark", playUrl, header,"video")).octet().header(header).string();
         }
 
 
