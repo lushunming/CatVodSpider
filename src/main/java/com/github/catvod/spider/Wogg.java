b671b0d871647e345cf2ea828a931b9936e9c255
lushunming <1357197829@qq.com>
2026-04-23 08:08:55 +0800

    玩偶哥哥修复

--- src/main/java/com/github/catvod/spider/Wogg.java
+++ src/main/java/com/github/catvod/spider/Wogg.java
@@ -27,7 +27,7 @@ import java.util.regex.Pattern;
  */
 public class Wogg extends Cloud {
 
-    private final String siteUrl = "http://wogg.xxooo.cf/";
+    private final String siteUrl = "http://woggpan.333232.xyz/";
     private final Pattern regexCategory = Pattern.compile("/vodtype/(\\w+).html");
     private final Pattern regexPageTotal = Pattern.compile("\\$\\(\"\\.mac_total\"\\)\\.text\\('(\\d+)'\\);");
 
