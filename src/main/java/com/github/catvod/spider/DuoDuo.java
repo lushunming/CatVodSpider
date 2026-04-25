59f0219d3a84959d036db625bc4c8a9ea8ab8e2b
lushunming <1357197829@qq.com>
2026-04-25 08:08:46 +0800

    玩偶哥哥修复

--- src/main/java/com/github/catvod/spider/DuoDuo.java
+++ src/main/java/com/github/catvod/spider/DuoDuo.java
@@ -24,6 +24,7 @@ import java.util.regex.Pattern;
 public class DuoDuo extends Cloud {
 
     private String siteUrl = "https://tv.yydsys.top/";
+
     private final Pattern regexCategory = Pattern.compile("index.php/vod/type/id/(\\w+).html");
     private final Pattern regexPageTotal = Pattern.compile("\\$\\(\"\\.mac_total\"\\)\\.text\\('(\\d+)'\\);");
 
